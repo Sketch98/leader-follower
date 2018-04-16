548b532be89629f7d9368a008878c4a03e203d67
Unknown <sketchn98@gmail.com>
2018-04-16 14:14:31 -0400

    testing stopping threads

--- repeated_timer.py
+++ repeated_timer.py
@@ -11,9 +11,13 @@ class RepeatedTimer:
         self._last_time = 0
         self._event = Event()
         self._thread = Thread(target=self._target)
+        self._thread.daemon = True
+        self._stopped = False
     
     def _target(self):
         while not self._event.wait(self._time()):
+            if self._stopped:
+                return
             time_elapsed = time() - self._last_time
             self._last_time += time_elapsed
             self._func(time_elapsed)
@@ -29,3 +33,24 @@ class RepeatedTimer:
     def stop(self):
         self._event.set()
         self._thread.join()
+
+
+if __name__ == '__main__':
+    from time import sleep
+    
+    
+    def say_hello(interval):
+        print('hello at {}s'.format(interval))
+    
+    
+    repeated_timer = RepeatedTimer(0.1, say_hello)
+    repeated_timer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        repeated_timer.stop()
+        print('OH NOOOO, IM MELTING')
+        sleep(1)
